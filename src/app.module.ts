1c10cfdf344a8bb335b4d1932d3bbba5b1753efa
Vlad Sh <kausvirtual@gmail.com>
2023-08-15 22:55:54 +0300

    added home base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { HomeModule } from './home/home.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, ConfigModule.forRoot()],
+  imports: [UserModule, PrismaModule, ConfigModule.forRoot(), HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
